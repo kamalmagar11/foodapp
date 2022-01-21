14c38e127992e9d8805beeccde42ee4e9d57b8c1
kamalmagar11 <kamalmr988@gmail.com>
2022-01-21 13:38:35 +0545

    firt

---
+++ lib/home.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+           Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+           Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+           Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+           Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+           Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+           Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+           Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+           Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+          Text('flutter ne update'),
+
+        ],),
+      ),
+    );
+  }
+}
